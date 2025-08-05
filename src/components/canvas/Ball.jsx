3b62928964a9cfb8471096c5ce4be5f15b4260e7
bhumi18 <bhumisadariya18@gmail.com>
2025-08-05 19:00:34 -0400

    feat: Add continuous animation to tech skill balls

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,5 +1,5 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import {
   Decal,
   Float,
@@ -12,23 +12,73 @@ import CanvasLoader from "../Loader";
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
+  const meshRef = useRef();
+
+  // Add continuous slow rotation animation
+  useFrame((state) => {
+    if (meshRef.current) {
+      // Simple left-right rotation on Y-axis only
+      meshRef.current.rotation.y = Math.sin(state.clock.elapsedTime * 0.5) * 0.3;
+    }
+  });
 
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+    <Float speed={0.8} rotationIntensity={0.3} floatIntensity={0.8}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
+      <mesh ref={meshRef} castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
-          color="#fff8eb"
+          color="#ffffff"
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
         />
+        {/* Front */}
         <Decal
           position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
+          rotation={[0, 0, 0]}
+          scale={0.8}
+          map={decal}
+          flatShading
+        />
+        {/* Back */}
+        <Decal
+          position={[0, 0, -1]}
+          rotation={[Math.PI, 0, 0]}
+          scale={0.8}
+          map={decal}
+          flatShading
+        />
+        {/* Top */}
+        <Decal
+          position={[0, 1, 0]}
+          rotation={[-Math.PI / 2, 0, 0]}
+          scale={0.8}
+          map={decal}
+          flatShading
+        />
+        {/* Bottom */}
+        <Decal
+          position={[0, -1, 0]}
+          rotation={[Math.PI / 2, 0, 0]}
+          scale={0.8}
+          map={decal}
+          flatShading
+        />
+        {/* Left */}
+        <Decal
+          position={[-1, 0, 0]}
+          rotation={[0, Math.PI / 2, 0]}
+          scale={0.8}
+          map={decal}
+          flatShading
+        />
+        {/* Right */}
+        <Decal
+          position={[1, 0, 0]}
+          rotation={[0, -Math.PI / 2, 0]}
+          scale={0.8}
           map={decal}
           flatShading
         />
@@ -40,12 +90,17 @@ const Ball = (props) => {
 const BallCanvas = ({ icon }) => {
   return (
     <Canvas
-      frameloop="demand"
+      frameloop="always"
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} />
+        <OrbitControls 
+          enableZoom={false}
+          autoRotate={false}
+          enablePan={false}
+          enableRotate={false}
+        />
         <Ball imgUrl={icon} />
       </Suspense>
 
